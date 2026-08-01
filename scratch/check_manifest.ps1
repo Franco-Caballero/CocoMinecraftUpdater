7107ee6750ef430bfb95afccb6e1feb8d6a31a31
FrancoC <francoemilioc@gmail.com>
2026-08-01 19:54:14 -0400

    Publish Coco Pack 0.5.68

--- scratch/check_manifest.ps1
+++
@@ -1,2 +0,0 @@
-$manifest = Invoke-RestMethod -Uri "https://github.com/Franco-Caballero/CocoMinecraftUpdater/releases/latest/download/latest.json" -UseBasicParsing
-Write-Host "Published Version:" $manifest.version
--- scratch/check_tag68.ps1
+++
@@ -1,2 +0,0 @@
-$r = Invoke-RestMethod -Uri 'https://api.github.com/repos/Franco-Caballero/CocoMinecraftUpdater/releases/tags/v0.5.68' -UseBasicParsing
-Write-Host "TagName:" $r.tag_name "Draft:" $r.draft "ID:" $r.id
