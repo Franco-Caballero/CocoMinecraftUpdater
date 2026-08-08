b4ce6245d794c4c4182896e9ca3c29cf714a1493
FrancoC <francoemilioc@gmail.com>
2026-08-08 17:00:25 -0400

    Publish Coco Pack 0.5.80

--- fabric-mod/src/main/java/cl/coco/minecraft/CocoProtocol.java
+++ fabric-mod/src/main/java/cl/coco/minecraft/CocoProtocol.java
@@ -8,7 +8,7 @@ import net.minecraft.resources.Identifier;
 
 public final class CocoProtocol {
     public static final String PACK_ID = "coco-minecraft";
-    public static final String PACK_VERSION = "0.5.79";
+    public static final String PACK_VERSION = "0.5.80";
 
     private CocoProtocol() { }
 
