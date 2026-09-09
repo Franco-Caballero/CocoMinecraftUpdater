2b7b3e3a9390869b6f2aea9c4a076256c0438d40
FrancoC <francoemilioc@gmail.com>
2026-09-09 19:55:10 -0300

    Publish Coco Pack 0.5.163

--- engine/CocoLauncher.ps1
+++ engine/CocoLauncher.ps1
@@ -4463,10 +4463,6 @@ function Ensure-CocoOnlineFixSuppression([string]$InstanceRoot, $Experience){
             $realAppId = '323850'
             $hash0 = 'a395613e2ad6042eda047fb931a780454520bb6739d41dba032fdaf4d4c6625c4fd717a5aa6f750771a69659aee6038242a57e028bcb2f14e91f1f33010220bb'
             $hash1337 = 'c8b649463614f6fc40366d28c9e5750a8a1f818e267be3ff10195df6c7b7599bbc255c0195ef048d1774d979ce79d361fa593d44bf9c1641bdd92b6b3cd6c852'
-        }elseif($expId -eq 'worms-wmd' -or $appId -eq '327030'){
-            $realAppId = '327030'
-            $hash0 = '3653324c96e22689b64109ee818355da5f7bbd3c974a264c46e5acc81eac208a314f3c63a0a19f3cda322a799bf88b6154669757582d5421eee7e1be244108cc'
-            $hash1337 = '2b29cc6dd3ec262b49f4b39423b38e0e6d0ef1ecae74f2d50b7f229c825dd453bda52ab7979e68b9e21fc0b6d2dabdd5aab345724fbfd82513c42c44f8820941'
         }elseif($appId){
             $realAppId = $appId
             $hash0 = 'b4353c02359f2a29161f863d31d525227f958c269c51a920a5a6c14c37dbd0f0d9a0ede86cf0a35fa608ecccdfa1cbcc712d762d1cc62f3a64d74506c056a476'
@@ -4536,9 +4532,6 @@ function Ensure-CocoOnlineFixSuppression([string]$InstanceRoot, $Experience){
                 }elseif($content -match '(?i)RealAppId\s*=\s*323850'){
                     $targetHash0 = 'a395613e2ad6042eda047fb931a780454520bb6739d41dba032fdaf4d4c6625c4fd717a5aa6f750771a69659aee6038242a57e028bcb2f14e91f1f33010220bb'
                     $targetHash1337 = 'c8b649463614f6fc40366d28c9e5750a8a1f818e267be3ff10195df6c7b7599bbc255c0195ef048d1774d979ce79d361fa593d44bf9c1641bdd92b6b3cd6c852'
-                }elseif($content -match '(?i)RealAppId\s*=\s*327030'){
-                    $targetHash0 = '3653324c96e22689b64109ee818355da5f7bbd3c974a264c46e5acc81eac208a314f3c63a0a19f3cda322a799bf88b6154669757582d5421eee7e1be244108cc'
-                    $targetHash1337 = '2b29cc6dd3ec262b49f4b39423b38e0e6d0ef1ecae74f2d50b7f229c825dd453bda52ab7979e68b9e21fc0b6d2dabdd5aab345724fbfd82513c42c44f8820941'
                 }elseif(-not $targetHash1337){
                     if($content -match '(?i)1337\s*=\s*([a-f0-9]{128})'){
                         $targetHash1337 = $matches[1]
--- tests/Test-CocoLauncherCatalog.ps1
+++ tests/Test-CocoLauncherCatalog.ps1
@@ -118,7 +118,7 @@ if($valorantState.startItemAll-ne'shop:knife'-or
     throw 'La tienda de CSmain no contiene el arsenal Valorant y el cuchillo inicial fijados.'
 }
 $managedExperiences=@($catalog.experiences|Where-Object managementMode -eq 'managed')
-if($managedExperiences.Count-ne22-or
+if($managedExperiences.Count-ne21-or
     @($managedExperiences|Where-Object{$_.PSObject.Properties.Name-contains'compatibility'}).Count){
     throw 'Todas las experiencias deben estar visibles por presencia en catalogo, sin estados de bloqueo/experimento.'
 }
@@ -413,18 +413,6 @@ if($modRequired.Count-ne5-or`
 }
 if([string]$moveOrDie.runtimePolicies.defenderExclusion-ne'required'-or[string]$moveOrDie.runtimePolicies.onlineFixAppId-ne'323850'){
     throw 'Move or Die no declara sus politicas standalone de Defender y OnlineFix.'
-}
-$wormsWmd=@($catalog.experiences|Where-Object id -eq 'worms-wmd'|Select-Object -First 1)[0]
-if(-not$wormsWmd-or[string]$wormsWmd.runtime.executable-ne'Worms W.M.D.exe'-or$wormsWmd.runtime.type-ne'standalone'-or$wormsWmd.managementMode-ne'managed'){
-    throw 'La experiencia standalone Worms W.M.D no esta declarada correctamente.'
-}
-$wwmdRequired=@($wormsWmd.runtime.requiredFiles)
-if($wwmdRequired.Count-ne5-or`
-   @($wwmdRequired|Where-Object{[string]$_.sha256-notmatch'^[a-f0-9]{64}$'-or[int64]$_.size-le0-or[string]$_.archiveSha256-notin@($wormsWmd.pack.archives.sha256)}).Count){
-    throw 'Worms W.M.D no fija todos sus archivos base reparables por ruta, hash, tamano y archive exacto.'
-}
-if([string]$wormsWmd.runtimePolicies.defenderExclusion-ne'required'-or[string]$wormsWmd.runtimePolicies.onlineFixAppId-ne'327030'){
-    throw 'Worms W.M.D no declara sus politicas standalone de Defender y OnlineFix.'
 }
 $smolbird=@($catalog.globalPolicies.customSkinLoader.localSkins|Where-Object username -eq 'smolbird')
 if($smolbird.Count-ne1-or$smolbird[0].sha256-ne'fbfb5fdf0c1a71d3904efcbdfe9b403107c133b9137a302f1611e8adc29864fb'){
